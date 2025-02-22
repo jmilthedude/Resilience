62bd5222f38e56d7300330cc8bfbad22c6837b84
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2025-02-22 09:29:40 -0600

    some theme stuff

--- ui/src/form/user/UserLoginForm.tsx
+++ ui/src/form/user/UserLoginForm.tsx
@@ -1,8 +1,9 @@
 import React, {FormEvent, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {Button, Container, Input} from "@mantine/core";
+import {Button, Container, TextInput} from "@mantine/core";
 import useApi from "../../api/axiosInstance";
 import {useAuth} from "../../auth/AuthProvider";
+import styles from "./UserLoginForm.module.css"
 
 
 const LoginPage = () => {
@@ -37,25 +38,31 @@ const LoginPage = () => {
         <Container size="md" style={{marginTop: "50px", maxWidth: "400px"}}>
             <h2 style={{marginBottom: "5px"}}>Please Login</h2>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <Input style={{marginBottom: "15px"}} radius="lg"
-                           type="text"
-                           placeholder="Username"
-                           value={username}
-                           onChange={(e) => setUsername(e.target.value)}
-                           required
-                    />
-                </div>
-                <div>
-                    <Input radius="lg" style={{marginBottom: "15px"}}
-                           type="password"
-                           placeholder="Password"
-                           value={password}
-                           onChange={(e) => setPassword(e.target.value)}
-                           required
-                    />
-                </div>
-                <Button variant="filled" fullWidth={true} color="teal" radius="lg" type="submit">Login</Button>
+                <TextInput
+                    classNames={{
+                        input: styles.input
+                    }}
+                    style={{marginBottom: "15px"}}
+                    radius="lg"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <TextInput
+                    classNames={{
+                        input: styles.input
+                    }}
+                    radius="lg"
+                    style={{marginBottom: "15px"}}
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <Button variant="filled" fullWidth={true} color={"resilience"} radius="lg" type="submit">Login</Button>
             </form>
         </Container>
     );
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App";
-import {createTheme, MantineProvider, MantineThemeOverride} from "@mantine/core";
+import {createTheme, MantineProvider} from "@mantine/core";
 import "@mantine/core/styles.css"
 
 const rootElement = document.getElementById("root");
@@ -11,9 +11,24 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-const theme = createTheme({
 
-} as MantineThemeOverride);
+const theme = createTheme({
+        colors: {
+            resilience: [
+                '#f7ecff',
+                '#e7d6fb',
+                '#caaaf1',
+                '#ac7ce8',
+                '#9354e0',
+                '#833bdb',
+                '#7b2eda',
+                '#6921c2',
+                '#5d1cae',
+                '#501599'
+            ]
+        }
+    })
+;
 
 root.render(
     <MantineProvider theme={theme} defaultColorScheme={"dark"}>
